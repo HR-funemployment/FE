398d599400e2bad89c180dae0d07989ee38f91d3
David Zhang <david.zny1022@gmail.com>
2023-04-11 16:24:13 -0700

    copy in new eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: ['airbnb', 'airbnb/hooks', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['react', '@typescript-eslint'],
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+    project: path.resolve(__dirname, './tsconfig.json'),
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.tsx'] }],
+  },
+};
