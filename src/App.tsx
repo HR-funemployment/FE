ee1354224dd11c864a75b9705c5a101560754d67
Nathanael <nathanaeltjen@hotmail.com>
2023-04-13 12:25:54 -0700

    app placeholder 4/13/23

--- src/App.tsx
+++ src/App.tsx
@@ -2,28 +2,22 @@ import { useState } from 'react';
 import './styles/App.css';
 import Auth from './components/Auth';
 
-function App() {
-  // const [count, setCount] = useState(0);
-  const [user, setUser] = useState(null);
+interface User {
+  [key: string]: any;
+}
 
-  return (
-    <div className='App'>
-      <Auth />
-      {/* <h1>Vite + React</h1>
-      <div className='card'>
-        <button type='button' onClick={() => setCount((ct) => ct + 1)}>
-          count is
-          {count}
-        </button>
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className='read-the-docs'>Click on the Vite and React logos to learn more</p> */}
-    </div>
-  );
+interface AppProps {
+  setUser: (user: User) => void;
+}
+
+function PlaceHolder() {
+  return <div>PLACEHOLDER</div>;
+}
+
+function App() {
+  const [user, setUser] = useState<User | null>(null);
+
+  return <div className='App'>{user ? <PlaceHolder /> : <Auth setUser={setUser} />}</div>;
 }
 
 export default App;
